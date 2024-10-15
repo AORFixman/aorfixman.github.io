534e1e9eae530b27128bab933fd1c052a6eeaeb1
Rsyirah04 <rasyidahinsyi@gmail.com>
2024-10-15 11:54:22 +0800

    Resolved merge conflicts in links.php

--- lib/lightbox/links.php
+++ lib/lightbox/links.php
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 <?php
   $links = array(
     'css' => 'lib/lightbox/css/lightbox.min.css',
     'js' => 'lib/lightbox/js/lightbox.min.js'
   );
 ?>
-=======
-<?php
-  $links = array(
-    'css' => 'lib/lightbox/css/lightbox.min.css',
-    'js' => 'lib/lightbox/js/lightbox.min.js'
-  );
-?>
->>>>>>> 960405ccdfda35b82e20b2f7deb00ba321a22647
--- lib/waypoints/links.php
+++ lib/waypoints/links.php
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 <?php
   $links = array(
     'js' => 'lib/waypoints/waypoints.min.js'
   );
 ?>
-=======
-<?php
-  $links = array(
-    'js' => 'lib/waypoints/waypoints.min.js'
-  );
-?>
->>>>>>> 960405ccdfda35b82e20b2f7deb00ba321a22647
